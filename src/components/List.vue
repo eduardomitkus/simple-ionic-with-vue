f2f5f32acd6e994432d86715cb862e29623c6593
Eduardo Mitkus <eduardo.silva.mitkus@gmail.com>
2019-06-27 00:17:59 -0500

    Criampo funcionalidade de componente

---
+++ src/components/List.vue
@@ -0,0 +1,27 @@
+<template>
+  <ion-list>
+      <ion-item>
+        <ion-label>Pokémon Yellow</ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-label>Pokémon Yellow</ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-label>Pokémon Yellow</ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-label>Pokémon Yellow</ion-label>
+      </ion-item>
+    </ion-list>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    array: Array
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <List />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import List from '@/components/List'
 export default {
   name: 'home',
   components: {
-    HelloWorld
+      List
   }
+  
 }
 </script>
